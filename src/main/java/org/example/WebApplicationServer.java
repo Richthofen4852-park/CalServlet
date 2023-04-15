c95380c6e1c348326c5c4f42c2fac4b495354b33
Richthofen4852-park <smash87299@gmail.com>
2023-04-15 15:16:14 +0900

    계산기 서블릿 만들기

--- src/main/java/org/example/WebApplicationServer.java
+++ src/main/java/org/example/WebApplicationServer.java
@@ -4,9 +4,11 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 
+@SpringBootApplication
 public class WebApplicationServer {
 
     private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
